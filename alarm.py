b21f0b6e38f36e0629c15f63dd4ed353fdbc0b08
Matt <matt.desmarais78@gmail.com>
2018-08-26 16:45:31 -0400

    initial upload

---
+++ alarm.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+from twilio.rest import Client 
+from time import sleep
+import time
+import RPi.GPIO as GPIO
+from squid import *
+import datetime
+
+#numbers_to_message = ['+12345678910', '+11098765432']
+numbers_to_message = ['+12345678910'] #recipient(s)
+fromnumber = '+12345678910' #your twilio number
+account_sid = 'Twilio SID'
+auth_token = 'Twilio Token'
+client = Client(account_sid, auth_token)
+
+rgb = Squid(16, 20, 21) 
+GPIO.setmode(GPIO.BCM)
+
+BuzzerPin = 17 
+door_switch_pin = 18
+ 
+#Set up inputs
+GPIO.setup(door_switch_pin, GPIO.IN,  pull_up_down=GPIO.PUD_UP)
+GPIO.setup(BuzzerPin, GPIO.OUT)
+GPIO.output(BuzzerPin, GPIO.HIGH)
+
+#Set up variables
+prev_door = False
+start = None
+now = None
+alarm = False
+textsent = False
+resolvedtext = False
+
+#turn buzzer on
+def on():
+	GPIO.output(BuzzerPin, GPIO.LOW)
+
+#turn buzzer off
+def off():
+	GPIO.output(BuzzerPin, GPIO.HIGH)
+
+#main loop
+while True:
+	#Update sensor state each loop
+	door = GPIO.input(door_switch_pin)
+	if not door and not prev_door:
+                print "door closed"
+                rgb.set_color(GREEN)
+                off()
+                time.sleep(2)
+                #if alarm has been triggered send text that door has closed
+                if alarm:
+                        for number in numbers_to_message:
+                                client.messages.create(
+                                        body = 'Door Closed, freezer SEVEN at '+str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
+                                        from_ = fromnumber,
+                                        to = number
+                                )
+                alarm = False
+                textsent = False
+                resolvedtext = False
+        if door and not prev_door:
+		print "door opened"
+		start = time.time()
+                time.sleep(2)
+	if door and prev_door:
+		print "door still open"
+		now = time.time()
+                time.sleep(2)
+		elapsed = (now - start)/60
+                if alarm and not textsent:
+                        for number in numbers_to_message:
+                                client.messages.create(
+                                        body='Freezer Alarm on SEVEN at '+str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
+                                        from_=fromnumber,
+                                        to=number
+                                )
+                        textsent = True
+                if elapsed > 5:
+                        on()
+                        rgb.set_color(RED)
+			alarm = True
+                        continue
+		if elapsed > 4:
+			rgb.set_color(RED)
+			time.sleep(1)
+			rgb.set_color(OFF)
+                        continue
+		if elapsed > 3:
+                        rgb.set_color(GREEN)
+                        time.sleep(1)
+                        rgb.set_color(OFF)
+                if elapsed < 3:
+                        rgb.set_color(GREEN)
+        prev_door = door    
