20acacdda0d0b816910e02ac484ae1ca96a50c59
Matt <matt.desmarais78@gmail.com>
2018-09-06 20:13:38 -0400

    Update tempiodata.py

--- tempiodata.py
+++ tempiodata.py
@@ -1,29 +1,3 @@
-#!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
-
-# Can enable debug output by uncommenting:
-#import logging
-#logging.basicConfig(level=logging.DEBUG)
-
 import time
 
 import Adafruit_GPIO.SPI as SPI
@@ -41,22 +15,13 @@ client = MQTTClient(ADAFRUIT_IO_USERNAME, ADAFRUIT_IO_KEY)
 def c_to_f(c):
         return c * 9.0 / 5.0 + 32.0
 
-
-# Uncomment one of the blocks of code below to configure your Pi or BBB to use
-# software or hardware SPI.
-
 # Raspberry Pi software SPI configuration.
 CLK = 11
 CS  = 9
 DO  = 10
 sensor = MAX31855.MAX31855(CLK, CS, DO)
 
-# Raspberry Pi hardware SPI configuration.
-#SPI_PORT   = 0
-#SPI_DEVICE = 0
-#sensor = MAX31855.MAX31855(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
-
-# Loop printing measurements every second.
+# Loop printing measurements and uploading to IO about every minute
 print('Press Ctrl-C to quit.')
 while True:
     temp = sensor.readTempC()
