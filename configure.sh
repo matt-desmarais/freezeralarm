9b5b7349b139fadc181a5947c9e6e62447808671
Matt <matt.desmarais78@gmail.com>
2018-09-20 18:57:17 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -33,7 +33,7 @@ done;
 echo "Do you wish to setup temperature monitoring?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) sudo cp /home/pi/freezeralarm/tempiodata.service /etc/systemd/system; sudo systemctl enable tempiodata.service; echo "Enter Adafruit IO key"; read iokey; echo $iokey; sed -i "7s/IO KEY/$iokey/g" /home/pi/freezeralarm/tempiodata.py; echo "Enter Adafruit username"; read username; echo $username; sed -i "8s/user name/$username/g" /home/pi/freezeralarm/tempiodata.py; break;;
+        Yes ) sudo cp /home/pi/freezeralarm/tempiodata.service /etc/systemd/system; sudo systemctl enable tempiodata.service; echo "Enter Adafruit IO key"; read iokey; echo $iokey; sed -i "7s/IO KEY/$iokey/g" /home/pi/freezeralarm/tempiodata.py; echo "Enter Adafruit username"; read username; echo $username; sed -i "8s/user name/$username/g" /home/pi/freezeralarm/tempiodata.py; echo "Enter Feed name for IO example: freezer2"; read feed; echo $feed; sed -i "32s/freezer/$feed/g" /home/pi/freezeralarm/tempiodata.py;  break;;
         No ) exit;;
     esac
 done; 
