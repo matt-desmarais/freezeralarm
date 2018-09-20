f5e0c71fcf26768d83e690e04678ef6ef8781d7e
Matt <matt.desmarais78@gmail.com>
2018-09-20 15:53:24 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,8 @@
 sudo apt-get update
-sudo apt-get install build-essential python-dev python-pip python-smbus git scons swig python3-pip -y
-sudo pip install twilio
-sudo pip uninstall pyopenssl -y
-sudo apt-get install python-openssl
-sudo pip install datetime
-git clone https://github.com/simonmonk/squid.git
-cd squid
-sudo python setup.py install
+sudo apt-get install build-essential git scons swig python3-pip -y
 cd /home/pi/
 git clone https://github.com/matt-desmarais/freezeralarm.git
-sudo pip3 install adafruit-io RPi.GPIO twilio
+sudo pip3 install adafruit-io RPi.GPIO twilio datetime
 cd /home/pi/
 git clone https://github.com/adafruit/Adafruit_Python_MAX31855.git
 cd Adafruit_Python_MAX31855
@@ -17,4 +10,8 @@ sudo python3 setup.py install
 cd /home/pi/
 git clone https://github.com/adafruit/Adafruit_Python_GPIO.git
 cd Adafruit_Python_GPIO
+sudo python3 setup.py install
+cd /home/pi/
+git clone https://github.com/simonmonk/squid.git
+cd squid
 sudo python3 setup.py install
