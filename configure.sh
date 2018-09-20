930022ca9b6dd34de44bc3df904637bc9cad0bf0
Matt <matt.desmarais78@gmail.com>
2018-09-20 16:51:06 -0400

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -19,7 +19,7 @@ done
 echo "Do you wish to setup temperature alarms?"
 select yn in "Yes" "No"; do
     case $yn in
-        Yes ) echo "Enter temperature(F) threshold example 20 "; read temp; echo $temp; sed -i "25s/tempAlarmThreshold.*/tempAlarmThreshold = '$temp'/g" /home/pi/freezeralarm/tempalarm.py; echo "Enter duration over threshold to trigger alarm threshold example 20 "; read time; echo $time; sed -i "26s/tempAlarmDuration.*/tempAlarmDuration = '$time'/g" /home/pi/freezeralarm/tempalarm.py; sudo cp /home/pi/freezeralarm/tempalarm.service /etc/systemd/system; sudo systemctl enable tempalarm.service;  echo "Did you just setup a freezer alarm?"
+        Yes ) echo "Enter temperature(F) threshold example 20 "; read temp; echo $temp; sed -i "25s/tempAlarmThreshold.*/tempAlarmThreshold = $temp/g" /home/pi/freezeralarm/tempalarm.py; echo "Enter duration over threshold to trigger alarm threshold example 20 "; read time; echo $time; sed -i "26s/tempAlarmDuration.*/tempAlarmDuration = $time/g" /home/pi/freezeralarm/tempalarm.py; sudo cp /home/pi/freezeralarm/tempalarm.service /etc/systemd/system; sudo systemctl enable tempalarm.service;  echo "Did you just setup a freezer alarm?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) echo $twiliosid; sed -i "11s/account_sid.*/account_sid = '$twiliosid'/g" /home/pi/freezeralarm/tempalarm.py; echo $twiliotoken; sed -i "12s/auth_token.*/auth_token = '$twiliotoken'/g" /home/pi/freezeralarm/tempalarm.py; echo $twiliofrom; sed -i "10s/fromnumber.*/fromnumber = '+$twiliofrom'/g" /home/pi/freezeralarm/tempalarm.py; sed -i "9s/numbers_to_message.*/numbers_to_message = [$twiliomessages]/g" /home/pi/freezeralarm/tempalarm.py; echo "All entries copied over"; break;;
