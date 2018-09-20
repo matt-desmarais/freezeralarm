5288a488ac4a0aca5e8783e1425a8bed2a8560ef
Matt <matt.desmarais78@gmail.com>
2018-09-20 16:11:16 -0400

    Update singledooralarm.py

--- singledooralarm.py
+++ singledooralarm.py
@@ -1,4 +1,3 @@
-#!/usr/bin/env python
 from twilio.rest import Client 
 from time import sleep
 import time
@@ -49,10 +48,12 @@ def beep(x):
 while True:
     #Update sensor state each loop
     door = GPIO.input(door_switch_pin)
+    if not door and prev_door:
+        print("door closed")
     #detect if door is closed then turn led blue and 
     #if alarm went off send a closed door sms
     if not door and not prev_door:
-                print "door closed"
+                #print("door closed")
                 rgb.set_color(BLUE)
                 off()
                 #time.sleep(2)
@@ -67,46 +68,46 @@ while True:
                 alarm = False
                 textsent = False
                 resolvedtext = False
-        time.sleep(1)
+                time.sleep(1)
     #detect if door has been opened 
     #if door opened start timer turn led green
-        if door and not prev_door:
-        print "door opened"
-                rgb.set_color(GREEN)
-        start = time.time()
-                #time.sleep(2)
+    if door and not prev_door:
+            print("door opened")
+            rgb.set_color(GREEN)
+            start = time.time()
+            #time.sleep(2)
     #detect if door is still opened 
     #if alarm has been triggered send sms
     if door and prev_door:
-        print "door still open"
-        now = time.time()
+            #print("door still open")
+            now = time.time()
+            time.sleep(1)
+            elapsed = (now - start)/60
+            if alarm and not textsent:
+                    for number in numbers_to_message:
+                            client.messages.create(
+                            body='Freezer Door Alarm at '+str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
+                            from_=fromnumber,
+                            to=number
+                        )
+                    textsent = True
+            #if 5min passed, activate alarm/sms
+            if elapsed > 5:
+                on()
+                rgb.set_color(RED)
+                alarm = True
+                continue
+            #if 4min passed, flash led red and beep
+            if elapsed > 4:
+                beep(0.5)
+                rgb.set_color(RED)
                 time.sleep(1)
-        elapsed = (now - start)/60
-                if alarm and not textsent:
-                        for number in numbers_to_message:
-                                client.messages.create(
-                                        body='Freezer Door Alarm at '+str(datetime.datetime.now().strftime("%Y-%m-%d %H:%M")),
-                                        from_=fromnumber,
-                                        to=number
-                                )
-                        textsent = True
-        #if 5min passed, activate alarm/sms
-        if elapsed > 5:
-            on()
-            rgb.set_color(RED)
-            alarm = True
-            continue
-        #if 4min passed, flash led red and beep
-        if elapsed > 4:
-            beep(0.5)
-            rgb.set_color(RED)
-            time.sleep(1)
-            rgb.set_color(OFF)
-            continue
-        #if 3 min passed, flash led green
-        if elapsed > 3:
-            rgb.set_color(GREEN)
-            time.sleep(1)
-            rgb.set_color(OFF)
-            time.sleep(1)
-        prev_door = door  
+                rgb.set_color(OFF)
+                continue
+            #if 3 min passed, flash led green
+            if elapsed > 3:
+                rgb.set_color(GREEN)
+                time.sleep(1)
+                rgb.set_color(OFF)
+                time.sleep(1)
+    prev_door = door  
