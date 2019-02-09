762b6462355b878cdd8390925abf2ecf32703a73
Matt <matt.desmarais78@gmail.com>
2019-02-09 00:52:35 -0500

    Update tempalarm.py

--- tempalarm.py
+++ tempalarm.py
@@ -6,10 +6,10 @@ from twilio.rest import Client
 import datetime
 
 #Twilio Credentails/setup
-numbers_to_message = ['+12345678910']
-fromnumber = '+12345678910'
-account_sid = 'SID'
-auth_token = 'Token'
+numbers_to_message = ['+1234567890']
+fromnumber = '+1234567890'
+account_sid = 'twilio sid'
+auth_token = 'twilio token'
 client = Client(account_sid, auth_token)
 
 # Define a function to convert celsius to fahrenheit.
@@ -22,9 +22,11 @@ CS  = 9
 DO  = 10
 sensor = MAX31855.MAX31855(CLK, CS, DO)
 
-tempAlarmThreshold = 20
+isnan = False
+tempAlarmThreshold = 0
 tempAlarmDuration = 20
 textsent = False
+previousTemp = None
 
 print('Press Ctrl-C to quit.')
 while True:
@@ -32,13 +34,27 @@ while True:
     temp = sensor.readTempC()
     if math.isnan(temp):
         continue
+    if(previousTemp != None and abs(c_to_f(previousTemp)-c_to_f(temp)) > 10):
+        print("previous temp error")
+        previousTemp = temp
+        temp = sensor.readInternalC()
+        continue
     start = time.time()
-    while c_to_f(temp) > tempAlarmThreshold:
-        time.sleep(10)
+    while c_to_f(temp) > tempAlarmThreshold or math.isnan(temp):
         temp = sensor.readTempC()
+        print('first:'+str(c_to_f(temp)))
+        if math.isnan(temp):
+            print('continued')
+            continue
+        if(previousTemp != None and abs(c_to_f(previousTemp)-c_to_f(temp)) > 10):
+            print("previous temp error")
+            previousTemp = temp
+            temp = sensor.readTempC()
+            continue
+        time.sleep(3)
         now = time.time()
         elapsed = (now - start)/60
-        print('elapsed:'+str(elapsed))
+        print('elapsed:'+str(elapsed)+" Temp: "+str(c_to_f(temp)))
         if elapsed > tempAlarmDuration:
             print('Temperature Alarm')
             if not textsent:
@@ -52,4 +68,5 @@ while True:
     internal = sensor.readInternalC()
     print('Thermocouple Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(temp, c_to_f(temp)))
     print('    Internal Temperature: {0:0.3F}*C / {1:0.3F}*F'.format(internal, c_to_f(internal)))
+    previousTemp = temp
     time.sleep(2)
